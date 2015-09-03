45d6a1b893eb23195dbc98bebe03919fe0d80af5
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-09-03 23:07:44 +0100

    something decent to put on the web for now

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
 <html>
     <head>
         <title>Tom Clarke</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link type="text/css" rel="stylesheet" href="css/main.css"/>
+        <link rel="icon" href="res/img/favicon.png">
     </head>
     <body>
-        Website under construction.
+        <div class="main">
+            <img id="picofme" src="res/img/me_circle.png"/>
+            <h1>Thomas B Clarke</h1>
+            <h2>Computer Science student at the University of Birmingham</h2>
+            <p>Hi, I'm Tom, and I'm currently studying at the University of Birmingham to get a degree in Computer Science. As well as the programs and projects I do inside of my degree, I also enjoy working on things outside of my course, which very soon shall all be shown here. For now you can check out my <a href="http://linkedin.com/in/tombclarke">LinkedIn</a>, my <a href="res/files/CV.pdf">CV</a> or take a look at what I've done over summer at <a href="https://labs.majestic.com">Majestic Labs</a>. I'm looking forward to building my little corner of the internet into a portfolio of my work, and hope to see you again here soon.</p>
+            <p>The quickest way to contact me is via email: <a href="mailto:tomclarke96@hotmail.co.uk">Contact me!</a></p>
+        </div>
     </body>
 </html>
