dacbedbbf15c9aca63da30db02b64f337d165ba7
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-09-05 10:58:48 +0100

    added buttons to header

--- php/header.php
+++ php/header.php
@@ -1,4 +1,11 @@
 <div id="header">
-    <a href="<?php echo $serv_dir; ?>">Home</a> | 
-    <a href="<?php echo $serv_dir; ?>projects">Projects</a>
+    <span id="header-nav">
+        <a href="<?php echo $serv_dir; ?>">Home</a> | 
+        <a href="<?php echo $serv_dir; ?>projects">Projects</a>
+    </span>
+    <span id="header-social-links">
+        <a target="_blank" href="http://linkedin.com/in/tombclarke"><img src="<?php echo $serv_dir; ?>res/img/linkedin_logo.png"/></a>
+        <a target="_blank" href="http://twitter.com/tombclarke"><img src="<?php echo $serv_dir; ?>res/img/twitter_logo.png"/></a>
+        <a target="_blank" href="<?php echo $serv_dir; ?>res/files/CV.pdf"><img src="<?php echo $serv_dir; ?>res/img/cv.png"/></a>
+    </span>
 </div>
--- projects/index.php
+++ projects/index.php
@@ -9,12 +9,14 @@
     <body>
         <div class="main">
             <h1>Projects</h1>
-            <h2>Majestic Summer Internship 2015 - <a href="../majestic">View here</a></h2>
-            <div class="section">
-                <img class="project-img" src="../res/img/majesticseo-logo-black-white-large.png"/>
-                <p>Myself, <a target="_blank" href="http://mishawagner.co.nf">Misha Wagner</a> and Rowan Cole worked as a team at Majestic to create interesting data visualisations using D3.js. Majsetic also hired a journalist, <a target="_blank" href="https://twitter.com/pupulchatterjee">Pupul Chatterjee</a>, to write blogs about what we created. Through out the 10 weeks, we used HTML, CSS, PHP, Java, JavaScript with D3 and, of course, the Majestic API.</p>
-                <p>My favourite projects from the experience was 'Round The Clock', 'The Majestic Universe', 'MRacing' and (to be announced). While MRacing was built using canvas elements in HTML 5, all of the projects taught me a lot about how to handle the large amount of data we were getting back from the Majestic API, process it using JavaScript and display it using D3. MRacing was also a great learning curve as it was the largest game I'd ever worked on, along with using canvas elements which was also new to me.</p>
-                <p>I was also given the opportunity to go to Leeds Hack (leedshack.org) to represent Majestic as a sponsor, where I introduced Majestic to the attendees, was on hand to set hackers up with Majestic accounts, act as technical support and have discussions with them about what they could do as projects, and was responsible for selecting winner for our prizes and awarding them.</p>
+            <div class="project">
+                <h2>Majestic Summer Internship 2015 - <a href="../majestic">View here</a></h2>
+                <div class="section">
+                    <img class="project-img" src="../res/img/projects/majesticseo-logo-black-white-large.png"/>
+                    <p>Myself, <a target="_blank" href="http://mishawagner.co.nf">Misha Wagner</a> and Rowan Cole worked as a team at Majestic to create interesting data visualisations using D3.js. Majsetic also hired a journalist, <a target="_blank" href="https://twitter.com/pupulchatterjee">Pupul Chatterjee</a>, to write blogs about what we created. Through out the 10 weeks, we used HTML, CSS, PHP, Java, JavaScript with D3 and, of course, the Majestic API.</p>
+                    <p>My favourite projects from the experience was 'Round The Clock', 'The Majestic Universe', 'MRacing' and (to be announced). While MRacing was built using canvas elements in HTML 5, all of the projects taught me a lot about how to handle the large amount of data we were getting back from the Majestic API, process it using JavaScript and display it using D3. MRacing was also a great learning curve as it was the largest game I'd ever worked on, along with using canvas elements which was also new to me.</p>
+                    <p>I was also given the opportunity to go to Leeds Hack (leedshack.org) to represent Majestic as a sponsor, where I introduced Majestic to the attendees, was on hand to set hackers up with Majestic accounts, act as technical support and have discussions with them about what they could do as projects, and was responsible for selecting winner for our prizes and awarding them.</p>
+                </div>
             </div>
         </div>
     </body>
