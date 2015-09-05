90f0dd4bb69181dbc40c2fb81fdfc934c1887d80
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-09-05 11:19:36 +0100

    new home page txt

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
             <h1>Thomas B Clarke</h1>
             <h2>Computer Science student at the University of Birmingham</h2>
             <div class="section">
-                <p>Hi, I'm Tom, and I'm currently studying at the University of Birmingham to get a degree in Computer Science. As well as the programs and projects I do inside of my degree, I also enjoy working on things outside of my course, which is what I want to show here. You can check out my <a href="http://linkedin.com/in/tombclarke">LinkedIn</a> and my <a href="res/files/CV.pdf">CV</a>.</p>
+                <p>Hi, I'm Tom, and I'm currently studying at the University of Birmingham to get a degree in Computer Science. As well as the programs and projects I do inside of my degree, I also enjoy working on things outside of my course, which is what I want to show here. Currently, I'm looking for a placement year for the 3rd year of my degree (starting summer 2016) and relevant part time work while at university this year.</p>
+                <p>So far, I have had experience in Java, OCaml, various web technologies, and a little bit of C# working in Unity. With this experience I feel ready and excited to go onto larger projects in the future, learning more languages and doing more interesting things!</p>
+                <p>You can check out my <a href="http://linkedin.com/in/tombclarke">LinkedIn</a>, <a href="res/files/CV.pdf">CV</a> and projects all from here.</p>
                 <p>The quickest way to contact me is via email: <a href="mailto:tomclarke96@hotmail.co.uk">Contact me!</a></p>
             </div>
         </div>
