3a3e0656b3ca804f20ef1b336335efe2d713627e
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-10-16 23:07:13 +0100

    removed hhg

--- subs/hhg/js/coreimports.js
+++
@@ -1,5 +0,0 @@
-function switchArrow(txt) {
-    //▼;
-    //▲;
-    return txt.substring(0, txt.length - 1) + ((txt.indexOf("▼") > -1) ? "▲" : "▼");
-}
--- subs/hhg/php/fb2.php
+++
@@ -1,9 +0,0 @@
-<div id="fb-custom">
-    <div class="fb-page" data-href="https://www.facebook.com/HinckleyHomelessLawrenceHouse12" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/HinckleyHomelessLawrenceHouse12"><a href="https://www.facebook.com/HinckleyHomelessLawrenceHouse12">Hinckley Homeless Group</a></blockquote></div></div>
-</div>
-
-<script>
-    setTimeout(function() {
-        $('#fb-custom').fadeIn();
-    }, 2000);
-</script>
