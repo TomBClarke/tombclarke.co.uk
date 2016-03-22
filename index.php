ecd19a23bb1390ff55e5f161faa74d1d190ba305
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-03-22 17:37:22 +0000

    added team project, updated home page

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
             <h2>Computer Science Student / Developer</h2>
             <h4>University of Birmingham</h4>
             <div class="section">
-                <p>Hi, I'm Tom, and I'm currently studying at the University of Birmingham to get a degree in Computer Science. As well as the programs and projects I do inside of my degree, I also enjoy working on things outside of my course, which is what I want to show here. Currently, I'm looking for a placement year for the 3rd year of my degree (starting summer 2016) and relevant part time work while at university this year.</p>
+                <p>Hi, I'm Tom, and I'm currently studying at the University of Birmingham to get a degree in Computer Science. As well as the programs and projects I do inside of my degree, I also enjoy working on things outside of my course, which is what I want to show here. I have also secured a industrial placement at <a href="https://www.cgi.com/en">CGI</a> starting summer 2016.</p>
                 <p>So far, I have had experience in Java, C# (used in Unity), MATLAB, OCaml, Haskell, HTML, CSS, JavaScript (including jQuery and D3.js), PHP and SQL. With this experience I feel ready and excited to go onto larger projects in the future, learning more languages and doing more interesting things!</p>
                 <p>You can check out my <a href="http://linkedin.com/in/tombclarke">LinkedIn</a>, <a href="res/files/CV.pdf">CV</a> and projects all from here.</p>
-                <p>The quickest way to contact me is via email: <a href="mailto:tomclarke96@hotmail.co.uk">Contact me!</a></p>
+                <p>The quickest way to contact me is via email: <a href="mailto:tom@tombclarke.co.uk">Contact me!</a></p>
             </div>
         </div>
     </body>
