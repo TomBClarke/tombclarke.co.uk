04f86d84c77f1e2d24eff6cb376682889980ebd2
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-09-29 14:25:18 +0100

    Meta tidying

--- php/coreimports.php
+++ php/coreimports.php
@@ -4,21 +4,19 @@
 <meta name="theme-color" content="#202020">
 <meta name="msapplication-navbutton-color" content="#202020">
 <meta name="apple-mobile-web-app-status-bar-style" content="#202020">
+<link type="text/plain" rel="author" href="http://www.tombclarke.co.uk/humans.txt" />
 <meta property="og:url"           content="http://www.tombclarke.co.uk/" />
 <meta property="og:type"          content="website" />
 <meta property="og:title"         content="Tom Clarke" />
 <meta property="og:description"   content="Hi, I'm Tom, I study Computer Science at the University of Birmingham, and am looking for great projects to work on." />
 <meta property="og:image"         content="http://www.tombclarke.co.uk/res/img/me_circle.png" />
-<meta name="url"                  content="http://www.tombclarke.co.uk/" />
-<meta name="type"                 content="website" />
-<meta name="title"                content="Tom Clarke" />
-<meta name="description"          content="Hi, I'm Tom, I study Computer Science at the University of Birmingham, and am looking for great projects to work on." />
-<meta name="image"                content="http://www.tombclarke.co.uk/res/img/me_circle.png" />
 <meta name="keywords"             content="TomBClarke, Tom B Clarke, Tom Clarke, Computer Science, projects, work, student, university, year in industry, developer">
 
 <!-- Setup dir -->
-<!-- This is to support some aweful code I wrote when I began this project, but is effort to remove. I guess it could in theory be useful at some point... -->
-<?php $serv_dir = "/tombclarke/"; ?>
+<?php
+    // This is to support some aweful code I wrote when I began this project, but is effort to remove. I guess it could in theory be useful at some point...
+    $serv_dir = "/tombclarke/"; 
+?>
 
 <script> var serv_dir = "<?php print $serv_dir; ?>"; </script>
 
