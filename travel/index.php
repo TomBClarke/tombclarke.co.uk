44c85eeb1a7fd4c344fa9faf0791d9208a8972c0
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-09-29 09:30:15 +0100

    Added what could be eventually a travel page...

---
+++ travel/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Travelling</title>
+        <?php $comp_dir = "../"; include $comp_dir . 'php/coreimports.php'; ?>
+    </head>
+    <body>
+        <?php include $comp_dir . 'php/pageimports.php'; ?>
+        <div class="main">
+            I enjoy travelling, so I'm going to save some memories here eventually...
+        </div>
+    </body>
+</html>
