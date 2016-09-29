0df4edb5a377bf508f70997c8ad952f2fff6ed09
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-09-29 12:31:30 +0100

    Because why not, the JSON parsing is now in PHP

--- travel/index.php
+++ travel/index.php
@@ -4,15 +4,46 @@
     <head>
         <title>Travelling</title>
         <?php $comp_dir = "../"; include $comp_dir . 'php/coreimports.php'; ?>
-        <script> var gotoplace = "<?php if(isset($_GET["place"])) { echo $_GET["place"]; } ?>";</script>
-        <script src="<?php echo $serv_dir; ?>js/travel.js"></script>
     </head>
-    <body onload="setupTravelPage();">
+    <body>
         <?php include $comp_dir . 'php/pageimports.php'; ?>
+        
+        <?php
+            $showingPlace = false;
+            $place = null;
+        
+            if(isset($_GET["place"])) {
+                $rawjson = file_get_contents($comp_dir . 'res/content/travel.json');
+                $places = json_decode($rawjson);
+                $placesList = $places->places;
+                foreach ($placesList as $placeTest) {
+                    if ($placeTest->id == $_GET["place"]) {
+                        $place = $placeTest;
+                        break;
+                    }
+                }
+            }
+        
+            if ($place == null) {
+        ?>
+        
         <div class="main">
             <h1>Travelling is fantastic!</h1>
-            <h2>I really enjoy travelling, and to help me remember I'm building this part of my website to share to fantastic times and places.</h2>
+            <h2>I really enjoy travelling, and to help me remember where I've been I'm building this part of my website to share to fantastic times and places.</h2>
             <h2>Feel free to take a look at...</h2>
         </div>
+        
+        <?php
+            } else {
+        ?>
+        
+        <div class="main">
+            <h1><?php echo $place->place; ?></h1>
+            <h2>I was here <?php echo $place->visited; ?>.</h2>
+        </div>
+        
+        <?php
+            }
+        ?>
     </body>
 </html>
