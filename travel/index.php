e9de5a52d62cef5d41bc186a8e6d42ca554e14d2
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-09-29 12:38:32 +0100

    Moved PHP so I have a fancy custom title

--- travel/index.php
+++ travel/index.php
@@ -1,31 +1,37 @@
 <!DOCTYPE html>
 
+<?php
+    $comp_dir = "../";
+    $place = null;
+        
+    if(isset($_GET["place"])) {
+        $rawjson = file_get_contents($comp_dir . 'res/content/travel.json');
+        $places = json_decode($rawjson);
+        $placesList = $places->places;
+        foreach ($placesList as $placeTest) {
+            if ($placeTest->id == $_GET["place"]) {
+                $place = $placeTest;
+                break;
+            }
+        }
+    }
+
+    if ($place == null) {
+        $pageTitle = "Travelling";
+    } else {
+        $pageTitle = $place->place;
+    }
+?>
+
 <html>
     <head>
-        <title>Travelling</title>
-        <?php $comp_dir = "../"; include $comp_dir . 'php/coreimports.php'; ?>
+        <title><?php echo $pageTitle; ?></title>
+        <?php include $comp_dir . 'php/coreimports.php'; ?>
     </head>
     <body>
         <?php include $comp_dir . 'php/pageimports.php'; ?>
         
-        <?php
-            $showingPlace = false;
-            $place = null;
-        
-            if(isset($_GET["place"])) {
-                $rawjson = file_get_contents($comp_dir . 'res/content/travel.json');
-                $places = json_decode($rawjson);
-                $placesList = $places->places;
-                foreach ($placesList as $placeTest) {
-                    if ($placeTest->id == $_GET["place"]) {
-                        $place = $placeTest;
-                        break;
-                    }
-                }
-            }
-        
-            if ($place == null) {
-        ?>
+        <?php if ($place == null) { // Main (start) page. ?>
         
         <div class="main">
             <h1>Travelling is fantastic!</h1>
@@ -33,9 +39,7 @@
             <h2>Feel free to take a look at...</h2>
         </div>
         
-        <?php
-            } else {
-        ?>
+        <?php } else { // Content for a place area. ?>
         
         <div class="main">
             <h1><?php echo $place->place; ?></h1>
