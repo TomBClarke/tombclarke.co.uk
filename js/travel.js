c16a4ba5a727245ab3e45ca35db62aa4f68d43dd
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-09-29 11:26:00 +0100

    JSON and JS filler code setup

--- js/travel.js
+++ js/travel.js
@@ -6,9 +6,26 @@ function setupTravelPage() {
 }
 
 function showPlace(place) {
-    var mainDiv = $('.main');
-    // Given all is good, clear the page first...
-    mainDiv.empty();
-    
-    mainDiv.append($('<h1></h1>').text(place));
+    $.ajax({
+        url: serv_dir + "res/content/travel.json",
+        dataType: "json",
+        success: function(places) {
+            var visitedPlaces = places.places;
+            var display = null;
+            for(var i = 0; i < visitedPlaces.length; i++) {
+                if (visitedPlaces[i].id == place) {
+                    display = visitedPlaces[i];
+                    i = visitedPlaces.length;
+                }
+            }
+            
+            if (!display) {
+                return;
+            }
+            
+            var mainDiv = $('.main');
+            mainDiv.empty();
+            mainDiv.append($('<h1></h1>').text(display.place));
+        }
+    });
 }
