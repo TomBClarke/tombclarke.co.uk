9ed36c0b75e5629ad3c33bc86f0e594118c44873
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-09-29 09:25:39 +0100

    Updated front page

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
             <h2>Computer Science Student / Developer</h2>
             <h4>University of Birmingham</h4>
             <div class="section">
-                <p>Hi, I'm Tom, and I'm currently studying at the University of Birmingham to get a degree in Computer Science. As well as the programs and projects I do inside of my degree, I also enjoy working on things outside of my course, which is what I want to show here. I have also secured a industrial placement at <a href="https://www.cgi.com/en">CGI</a> starting summer 2016.</p>
-                <p>So far, I have had experience in Java, C# (used in Unity), MATLAB, OCaml, Haskell, HTML, CSS, JavaScript (including jQuery and D3.js), PHP and SQL. With this experience I feel ready and excited to go onto larger projects in the future, learning more languages and doing more interesting things!</p>
+                <p>Hi, I'm Tom, and I'm currently studying at the University of Birmingham to get a degree in Computer Science. As well as the programs and projects I do inside of my degree, I also enjoy working on things outside of my course, which is what I want to show here. More accurately, I am currently progressing through my year in industry as part of my degree at <a href="https://www.cgi.com/en">CGI</a> until mid 2017.</p>
+                <p>So far, I have had experience in Java, JavaScript (including jQuery and D3.js), C# (used in Unity), MATLAB, OCaml, Haskell and other web technologies. With this experience I feel ready and excited to go onto larger projects in the future, learning more languages and doing more interesting things!</p>
                 <p>You can check out my <a href="http://linkedin.com/in/tombclarke">LinkedIn</a>, <a href="res/files/CV.pdf">CV</a> and projects all from here.</p>
                 <p>The quickest way to contact me is via email: <a href="mailto:tom@tombclarke.co.uk">Contact me!</a></p>
             </div>
